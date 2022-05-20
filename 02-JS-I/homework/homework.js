2038bf825f2e99f1fc5cbfc3eda9d23e7ca72b6a
mariaisaa <mariacifuentescc@gmail.com>
2022-05-20 14:56:00 -0700

    second commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -113,8 +113,10 @@ mayorQueCincuenta;
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+
+    return x % y;
 }
+obtenerResto;
 
 
 function esPar(num) {
@@ -143,6 +145,7 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+return num ** 2;
   
 }
 elevarAlCuadrado;
@@ -150,45 +153,66 @@ elevarAlCuadrado;
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+  return num ** 3;
 
 }
+elevarAlCubo;
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-
+return num ** exponent;
 }
+elevar;
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
+ return (Math.round(num));
 
 }
+redondearNumero; 
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
+  return (Math.ceil(num));
 
 }
+redondearHaciaArriba;
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-
+return (Math.Random(0, 1))
 }
+numeroRandom;
 
 function esPositivo(numero) {
   //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo.
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
+   if (numero > 0){
+     return "Es positivo";
+   }
+   else if (numero < 0) {
+     return "Es negativo"
+   }
+     else (numero == 0); {
+     return false;
+   }
 
 }
+esPositivo;
+
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  
+
 }
 
 function combinarNombres(nombre, apellido) {
@@ -208,28 +232,31 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-
+  return alto * ancho;
 }
+obtenerAreaRectangulo;
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-
+  return lado + lado + lado + lado
 }
+retornarPerimetro;
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+  return ((base * altura) / 2 )
 
 }
-
 
 function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
+  return euro * 1.20;
 
 }
 
@@ -240,6 +267,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+  
 
 }
 
